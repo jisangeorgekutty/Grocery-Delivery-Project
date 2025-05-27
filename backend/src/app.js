fdfaa1f5d01db53f32b97d0f248a6d643d893e9e
jisangeorgekutty <jisangeorge123@gmail.com>
2025-05-27 15:56:55 +0530

    An base express app created.

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT=process.env.PORT;
+const app = express();
+
+
+
+app.get('/',(req,res)=>{
+    res.send("API is Working");
+})
+
+app.listen(PORT,()=>{
+    console.log("Server Running On Port : "+PORT);
+})
